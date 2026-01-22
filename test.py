ca70cc670ad5dd243e3de3625e9bca4b28b15888
charleswaithaka24 <charleswaithaka24@gmail.com>
2026-01-23 01:21:18 +0300

    editting files

--- test.py
+++ test.py
@@ -1,22 +1,32 @@
-from pathlib import Path
-from collections import defaultdict
-direction=Path(input("input file path:"))
-files_by_ext=defaultdict(list)
-for current in direction.iterdir():
- if current.is_file():
-  ext=current.suffix
-  stats=current.stat()
-  size=stats.st_size
-  files_by_ext[ext].append(current.name)
-  files_by_ext[ext].append(f"{size} bytes")
-  print(current.name,size)
-for e, n in files_by_ext.items():
-   print(e," ", n)
-total=sum(1 for folder in direction.iterdir() )
-print("Total number of files :",total)
+try:
+         from pathlib import Path
+         from collections import defaultdict
+         direction=Path(input("input file path:"))
+         files_by_ext=defaultdict(list)
+         for current in direction.iterdir():
+            if current.is_file():
+               ext=current.suffix
+               stats=current.stat()
+               size =stats.st_size
+               files_by_ext[ext].append((current.name,size))
+              # print(current.name,size)
+         print()
+         print("Files by their extensions")
+         for e, n in files_by_ext.items():
+            print(e,)
+            print(" ",n )
+         total=sum(1 for folder in direction.iterdir() )
+         print()
+         print("TOTAL FILE SUMMARY")
+         for ext,files in files_by_ext.items():
+          largest_file=sorted(files,key=lambda x:x[1],reverse=True)
+          top=largest_file[:5]
+          print()
+          print(ext)
+          print("Largest files in each extension :",top)
+         print("Total number of files :",total)
 
-print("Total file size :",direction.stat().st_size,"bytes")
-largest_file=sorted(files_by_ext,key=lambda f:f[1])
-top_5=largest_file[:1]
-for top in top_5:
- print(largest_file)
+         print("Total file size :",direction.stat().st_size,"bytes")
+         
+except FileNotFoundError:
+    print("Invalid file path")
